8ad39301145974f8ec83d0880ca7f2160693281f
Vazgen <76519446+vazgenM2@users.noreply.github.com>
2021-12-30 21:09:40 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ let variable = {
 	number: '1234567890',
 	symbol: './"=+-*[]{}^&?()$%#@!;:><_~',
 }
-
+let time = []
+let t; // Timer
 function randomIntFromInterval(min, max) { // min and max included 
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -46,6 +47,40 @@ document.querySelector('.start-btn').addEventListener('click', () => {
 	if (!varMix.length) varMix = ['lowercase', 'uppercase']
 
 	level = document.querySelector('.level-inp').value
+	if (document.querySelector('.select-time').value == 'timer') {
+		time = [0, 0]
+		let value = +document.querySelector('.time-enter').value
+		if (value === 0) value = 5
+		if (value > 3600) value = 3600
+		while (value >= 60) {
+			value -= 60
+			time[0]++
+		}
+		time[1] = value
+		changeTimer(time[0], time[1])
+
+		t = setInterval(() => {
+			if (time[1]) time[1]--
+			else if (time[0]) {
+				time[0]--
+				time[1] = 59
+			}
+			else loseGame()
+			changeTimer(time[0], time[1])
+		}, 1000)
+	} else if (document.querySelector('.select-time').value == 'stopwatch') {
+		time = [0, 0]
+		changeTimer(time[0], time[1])
+
+		t = setInterval(() => {
+			if (time[1] < 59) time[1]++
+			else {
+				time[0]++
+				time[1] = 0
+			}
+			changeTimer(time[0], time[1])
+		}, 1000)
+	}
 
 	textLength = randomIntFromInterval((level - 1) * 10, level * 10)
 	if (textLength == 0) textLength = 1
@@ -69,7 +104,12 @@ document.addEventListener('keyup', (e) => {
 				document.querySelector('.correct').innerHTML = +document.querySelector('.correct').innerHTML + 1
 			} else {
 				document.querySelectorAll('.text-zone .variable')[ite].style.color = '#C41E3A'
-				document.querySelector('.failes').innerHTML = +document.querySelector('.failes').innerHTML + 1
+				if (document.querySelector('.select-mode').value == 'count') {
+					document.querySelector('.failes').innerHTML = +document.querySelector('.failes').innerHTML + 1
+				} else if (document.querySelector('.select-mode').value == 'lose') {
+					showMess('You lose :(', `${document.querySelector('.correct').innerHTML}/${textLength} is correct`, true)
+					backToHome()
+				}
 			}
 			ite++
 			for (let i = 0; i < document.querySelectorAll('.text-zone .variable').length; i++) {
@@ -77,20 +117,23 @@ document.addEventListener('keyup', (e) => {
 			}
 			if (ite !== textLength) document.querySelectorAll('.text-zone .variable')[ite].classList.add('active')
 		} else if (ite == textLength && e.key.length === 1) {
-			alert(Math.floor((+document.querySelector('.correct').innerHTML / ite) * 100) + '% correct')
-			document.querySelector('.board').style.display = 'none'
-			document.querySelector('main').style.display = 'flex'
+			let percent = Math.floor((+document.querySelector('.correct').innerHTML / ite) * 100)
+			if (percent == 100) showMess('Nice job !', 'All is correct', true)
+			else if (percent >= 65) showMess('Not bad', `${percent}% is correct`, true)
+			else if (percent >= 35) showMess('Try another time', `${percent}% is correct`, true)
+			else showMess('work more', `${percent}% is correct`, true)
+			backToHome()
 		}
 	}
 })
-
-
 // ========= Back button
-document.querySelector('.back-btn').addEventListener('click', () => {
+document.querySelector('.back-btn').addEventListener('click', backToHome)
+
+function backToHome() {
 	game = false
 	document.querySelector('.board').style.display = 'none'
 	document.querySelector('main').style.display = 'flex'
-})
+}
 
 // ================== Setting button
 document.querySelector('.setting').addEventListener('click', () => {
@@ -101,9 +144,13 @@ document.querySelector('.close-modal').addEventListener('click', () => {
 	document.querySelector('.settings').style.display = 'none'
 })
 
+// ================= Modal closing
+document.querySelector('.message-modal').addEventListener('click', () => {
+	document.querySelector('.modal').style.display = 'none'
+})
+
 
 // ==================== Variables chooseing
-
 for (let i = 0; i < document.querySelectorAll('.var').length; i++) {
 	document.querySelectorAll('.var')[i].addEventListener('change', e => {
 		isVariable[document.querySelectorAll('.var')[i].id] = document.querySelectorAll('.var')[i].checked
@@ -111,8 +158,38 @@ for (let i = 0; i < document.querySelectorAll('.var').length; i++) {
 }
 
 // ================== Chossing time
-
 document.querySelector('.select-time').addEventListener('change', () => {
-	if (document.querySelector('.select-time').value == 'none') document.querySelector('.time-enter-set').style.display = 'none'
-	else document.querySelector('.time-enter-set').style.display = 'block'
+	if (document.querySelector('.select-time').value == 'timer') document.querySelector('.time-enter-set').style.display = 'block'
+	else document.querySelector('.time-enter-set').style.display = 'none'
 })
+
+// ================== Function for changing timer value
+function changeTimer(min, sec) {
+	let m = String(min)
+	let s = String(sec)
+	if (m.length === 1) m = '0' + m
+	if (s.length === 1) s = '0' + s
+	document.querySelector('.timer').innerHTML = `${m}:${s}`
+}
+
+// ======================= Loseing game
+function loseGame() {
+	showMess('You lose :(', `${document.querySelector('.correct').innerHTML}/${textLength} is correct`, true)
+	backToHome()
+}
+
+// ============================= Show message
+
+function showMess(title, sub, clearTimer = false) {
+	if (clearTimer) clearInterval(t)
+	document.querySelector('.mess .general').innerHTML = title
+	document.querySelector('.mess .sub').innerHTML = sub
+	if (document.querySelector('.select-time').value == 'stopwatch') {
+		let m = String(time[0])
+		let s = String(time[1])
+		if (m.length === 1) m = '0' + m
+		if (s.length === 1) s = '0' + s
+		document.querySelector('.mess .sub').innerHTML += ` (${document.querySelector('.timer').innerHTML})`
+	}
+	document.querySelector('.modal').style.display = 'flex'
+}
